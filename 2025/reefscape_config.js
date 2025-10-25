80cf36b0cbe388ab3bf8c68ceeac44291dc701fc
team5458scouting <team5458scouting@gmail.com>
2025-10-24 17:23:38 -0700

    Update reefscape_config.js
    changed event code ^_^

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025camb",
+      "defaultValue": "2025caelk",
       "required": "true"
     },
     { "name": "Match Level",
